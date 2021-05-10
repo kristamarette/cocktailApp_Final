414e2a9582796d8b299d9be94d98cf60d720d795
kristamarette <55501269+kristamarette@users.noreply.github.com>
2021-05-10 18:56:25 -0500

    'All Recipes' page updated

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ let open = get("menu-btn");
 let nav = get("nav");
 let exit = get("exit-btn");
 
-
-
 open.addEventListener('click', ()=> {
     nav.classList.add('open-nav');
 })
@@ -17,6 +15,21 @@ exit.addEventListener('click', ()=> {
 })
 
 
-// --------- ALL RECIPES --------
+// -------- ALL RECIPES ---------
 
+	
+	$.getJSON('/pages/data.json', function(data) {
+		var output = '<div class="cards">';
+		$.each(data, function(key, val) {
+				output += '<h3>' + val.cocktailname + '</h3>';
+                output += '<ul>';
+                output += '<li>' + val.recipe + '</li>';
+                output += '<li>' + val.prep + '</li>';
+                output += '<li>' + 'Served: ' + val.served + '</li>';
+                output += '<li>' + 'Garnished with: ' +  val.garnish + '</li>';
+                output += '</ul>';
 
+			});
+		output += '</div>';
+		$('#cards').html(output);
+	}); //get JSON
