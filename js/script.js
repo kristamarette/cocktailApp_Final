3331bf85b0807b76265ba82c1b431a96a24e3a6d
kristamarette <55501269+kristamarette@users.noreply.github.com>
2021-05-10 21:07:30 -0500

    started Search Page

--- js/script.js
+++ js/script.js
@@ -33,3 +33,27 @@ exit.addEventListener('click', ()=> {
 		output += '</div>';
 		$('#cards').html(output);
 	}); //get JSON
+
+//---------- SEARCH PAGE ---------
+
+$('#search').keyup(function() {
+	var searchField = $('#search').val();
+	var myExp = new RegExp(searchField, "i");
+	$.getJSON('/pages/data.json', function(data) {
+		var output1 = '<div class="cards">';
+		$.each(data, function(key, val) {
+			if ((val.cocktailname.search(myExp) != -1) || val.prep.search(myExp) || val.garnish.search(myExp) || val.served.search(myExp) ||
+			(val.recipe.search(myExp) != -1)) {
+				output += '<h3>' + val.cocktailname + '</h3>';
+                output += '<ul>';
+                output += '<li>' + val.recipe + '</li>';
+                output += '<li>' + val.prep + '</li>';
+                output += '<li>' + 'Served: ' + val.served + '</li>';
+                output += '<li>' + 'Garnished with: ' +  val.garnish + '</li>';
+                output += '</ul>';
+			}
+		});
+		output += '</div>';
+		$('#update').html(output1);
+	}); //get JSON
+});
